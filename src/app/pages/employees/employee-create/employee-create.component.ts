75e783754683220f2308fb2cea752a89064cc3f5
Timothy jan <jan.timothy@gmail.com>
2025-07-25 12:16:52 -0700

    employee create complete

---
+++ src/app/pages/employees/employee-create/employee-create.component.ts
@@ -0,0 +1,80 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { SnackbarService } from '../../../services/snackbar.service';
+import { EmployeeService } from '../../../services/employee.service';
+import { InputComponent } from '../../../components/input/input.component';
+import { SelectDepartmentComponent } from '../../../components/select-department/select-department.component';
+import { SelectRoleComponent } from "../../../components/select-role/select-role.component";
+
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { InputSalaryComponent } from "../../../components/input-salary/input-salary.component";
+
+@Component({
+  selector: 'app-employee-create',
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatSelectModule,
+    InputComponent,
+    SelectDepartmentComponent,
+    SelectRoleComponent,
+    InputSalaryComponent
+],
+  templateUrl: './employee-create.component.html',
+  styleUrl: './employee-create.component.css',
+  standalone: true
+})
+export class EmployeeCreateComponent {
+  private _snackbar = inject(SnackbarService);
+  private _employeeService = inject(EmployeeService);
+
+  employeeForm: FormGroup = new FormGroup({
+    name: new FormControl("", [Validators.required, Validators.minLength(2), Validators.maxLength(100)]),
+    salary: new FormControl(0, [Validators.min(0), Validators.required]),
+    departmentId: new FormControl(-1, Validators.required),
+    roleId: new FormControl(-1, Validators.required)
+  });
+
+  constructor() {}
+
+  /** Handle department select changes */
+  handleDepartmentChange(departmentId: number): void {
+    this.employeeForm.controls["departmentId"].setValue(departmentId);
+  }
+
+  /** Handles role select changes */
+  handleRoleChange(roleId: number): void {
+    this.employeeForm.controls["roleId"].setValue(roleId);
+  }
+
+  /** Handles employee name change from input component and assigns name value to employeeForm */
+  handleEmployeeChange(employeeName: string): void {
+    this.employeeForm.controls["name"].setValue(employeeName.toUpperCase());
+  }
+
+  /** Handles salary change from input component and assigns salary value to employeeForm */
+  handleSalaryChange(salary: number): void {
+    // Convert to number with 2 decimal places
+    this.employeeForm.controls["salary"].setValue(Number(salary.toFixed(2)));
+  }
+
+  onSubmit(): void {
+    if (this.employeeForm.valid) {
+      const formValue = this.employeeForm.value;
+      console.log(formValue);
+      this._employeeService.addEmployee(formValue);
+      this._employeeService.notifyEmployeesChanged();
+      this.employeeForm.reset();
+      this._snackbar.success("Employee created.");
+    }
+    else {
+      this._snackbar.error("Employee failed to be created.");
+    }
+  }
+}
--- src/app/pages/employees/employees.component.ts
+++ src/app/pages/employees/employees.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { EmployeeCreateComponent } from "./employee-create/employee-create.component";
+import { EmployeeListComponent } from "./employee-list/employee-list.component";
 
 @Component({
   selector: 'app-employees',
-  imports: [],
+  imports: [EmployeeCreateComponent, EmployeeListComponent],
   templateUrl: './employees.component.html',
   styleUrl: './employees.component.css',
   standalone: true
