18bdf77889b3428f5ac895498981ddf09079ab0f
Timothy jan <jan.timothy@gmail.com>
2025-08-20 10:05:55 -0700

    Department-edit updated with form validation

--- src/app/components/dialogs/department-edit/department-edit.component.ts
+++ src/app/components/dialogs/department-edit/department-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject, Inject, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, Validators } from '@angular/forms';
 import { Department } from '../../../models/department.model';
 import { InputComponent } from '../../input/input.component';
 import { Subject } from 'rxjs';
@@ -32,8 +32,10 @@ export class DepartmentEditComponent implements OnInit, OnDestroy {
   private unsubscribe$ = new Subject<void>();
 
   isLoading: boolean = false;
-  originalDepartment: Department = { departmentId: -1, departmentName: "" };
-  editedDepartment: Department = { departmentId: -1, departmentName: "" };
+  departmentForm: FormGroup = new FormGroup({
+    departmentId: new FormControl(-1, [Validators.required, Validators.pattern(/^\d+$/)]),
+    departmentName: new FormControl("", [Validators.required, Validators.minLength(2), Validators.maxLength(50)])
+  });
 
   constructor(
     private dialogRef: MatDialogRef<DepartmentEditComponent>,
@@ -41,33 +43,40 @@ export class DepartmentEditComponent implements OnInit, OnDestroy {
   ) { }
 
   ngOnInit() {
-    this.getDepartmentById();
+    this.setDepartmentFormValues();
   }
 
-  /** Get Department */
-  getDepartmentById(): void {
+  /** Set departmentForm using getDepartmentById */
+  setDepartmentFormValues(): void {
+    this.isLoading = true;
     const dept = this._departmentService.getDepartmentById(this.data.departmentId);
-    if (!dept) {
-      console.error("Department not found");
-      this.dialogRef.close(null);
-      return;
-    }
-    this.originalDepartment = { ...dept };
-    this.editedDepartment = { ...dept };
+    this.departmentForm.patchValue({
+      departmentId: dept?.departmentId,
+      departmentName: dept?.departmentName
+    })
+    this.isLoading = false;
   }
 
   /** Handle department name input changes */
   onDepartmentNameChange(newValue: string): void {
-    this.editedDepartment.departmentName = newValue.toUpperCase();
+    this.departmentForm.controls["departmentName"].setValue(newValue.toUpperCase());
   }
 
   /** Save Changes */
   saveChanges(): void {
-    this.isLoading = true;
-    this._departmentService.updateDepartment(this.editedDepartment);
-    this._departmentService.notifyDepartmentsChanged();
-    this._snackbarService.success("Department saved.");
-    this.isLoading = false;
+    if(this.departmentForm.valid) {
+      this.isLoading = true;
+      const newDepartment = new Department(
+        this.departmentForm.controls["departmentId"].value,
+        this.departmentForm.controls["departmentName"].value
+      );
+      this._departmentService.updateDepartment(newDepartment);
+      this._departmentService.notifyDepartmentsChanged();
+      this._snackbarService.success("Department saved.");
+      this.isLoading = false;
+    } else {
+      this._snackbarService.error("Invalid department values");
+    }
   }
 
   /** Cancel and close dialog */
