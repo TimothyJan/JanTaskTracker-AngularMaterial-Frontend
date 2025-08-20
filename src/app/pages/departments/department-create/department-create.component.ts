9109e1a02625a36600c1b0af2a4d083d7bc292bb
Timothy jan <jan.timothy@gmail.com>
2025-08-19 17:43:06 -0700

    Department updated with unsubscribe and isLoading

--- src/app/pages/departments/department-create/department-create.component.ts
+++ src/app/pages/departments/department-create/department-create.component.ts
@@ -1,7 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 import { InputComponent } from '../../../components/input/input.component';
+import { Subject } from 'rxjs';
 
 import { SnackbarService } from '../../../services/snackbar.service';
 import { DepartmentService } from '../../../services/department.service';
@@ -23,10 +24,12 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrl: './department-create.component.css',
   standalone: true
 })
-export class DepartmentCreateComponent {
+export class DepartmentCreateComponent implements OnDestroy {
   private _snackbar = inject(SnackbarService);
   private _departmentService = inject(DepartmentService);
+  private unsubscribe$ = new Subject<void>();
 
+  isLoading: boolean = false;
   departmentForm: FormGroup = new FormGroup({
     departmentName: new FormControl("", [Validators.required, Validators.minLength(2), Validators.maxLength(50)])
   });
@@ -39,17 +42,26 @@ export class DepartmentCreateComponent {
   }
 
   onSubmit() {
+    this.isLoading = true;
     if (this.departmentForm.valid) {
       const departmentName = this.departmentForm.value.departmentName;
       if (!this._departmentService.checkDuplicates(departmentName)) {
         this._departmentService.createDepartment(this.departmentForm.value);
         this._departmentService.notifyDepartmentsChanged();
         this._snackbar.success("Department created.");
+        this.isLoading = false;
       } else {
         this._snackbar.error("Department already exists!");
+        this.isLoading = false;
       }
     } else {
       this._snackbar.warning("Department failed to be created.");
+      this.isLoading = false;
     }
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 }
